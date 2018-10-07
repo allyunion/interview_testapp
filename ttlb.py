f032e0e6b7ad6e3cd154b907b7059d9c5bbbd070
Jason Y. Lee <jylee@cs.ucr.edu>
2018-10-07 15:42:14 -0700

    Fix lint errors

--- ttlb.py
+++ ttlb.py
@@ -4,20 +4,22 @@
 
 import asyncio
 from pprint import pprint
-import json
 import time
 from urllib import request
 
-class ServerTest(object):
+class ServerTest:
     """Class to test the server"""
 
     def __init__(self, host, port, rps):
+        """ServerTest takes parameters of ServerTest(host, port, rps)
+           where rps is requests per second"""
         self.host = host
         self.port = port
         self.url = 'http://{}:{}'.format(host, port)
         self.rps = rps
-        
+
     async def execute_test(self):
+        """Execute the test"""
         try:
             with request.urlopen(self.url) as response:
                 start = time.time()
@@ -28,11 +30,11 @@ class ServerTest(object):
             return (504, '{}', 0)
 
     def do_tests(self):
+        """Loop continuously and sleep 1 second between each execution of
+           concurrent execute_tests * rps"""
         while True:
             loop = asyncio.get_event_loop()
             results = loop.run_until_complete(asyncio.gather(*[
                 self.execute_test() for i in range(0, self.rps)]))
             pprint(results)
             time.sleep(1)
-
-

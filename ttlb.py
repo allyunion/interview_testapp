eb4d9efad0e37ce48c5131ce65406a8039704101
Jason Y. Lee <jylee@cs.ucr.edu>
2018-10-07 20:58:23 -0700

    Add command line options.

--- ttlb.py
+++ ttlb.py
@@ -2,10 +2,12 @@
 
 """Demonstration of Time to Last Byte (TTLB)"""
 
+import argparse
 import asyncio
 from pprint import pprint
 import time
 from urllib import request
+from warnings import warn
 
 class ServerTest:
     """Class to test the server"""
@@ -36,5 +38,31 @@ class ServerTest:
             loop = asyncio.get_event_loop()
             results = loop.run_until_complete(asyncio.gather(*[
                 self.execute_test() for i in range(0, self.rps)]))
+            errors = [x[0] == 200 for x in results]
             pprint(results)
+            if False in errors:
+                warn("Warning: Received non-200 status code response!")
             time.sleep(1)
+
+def main():
+    """Main function"""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--host",
+        default="localhost",
+        help="Specify the host")
+    parser.add_argument(
+        "--port",
+        default=8080,
+        help="Specify the port")
+    parser.add_argument(
+        "--rps",
+        default=100,
+        help="Requests per second")
+    args = parser.parse_args()
+    tests = ServerTest(args.host, args.port, args.rps)
+    tests.do_tests()
+
+if __name__ == "__main__":
+    main()
